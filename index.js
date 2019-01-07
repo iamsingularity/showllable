0ee6ef660beaf338f35267a30f3d406196afcffa
victufell <victor.fellype.a@gmail.com>
2019-01-07 17:33:11 -0200

    add logic js and style css

---
+++ index.js
@@ -0,0 +1,51 @@
+function aniScrollable() {
+  var debounce = function(func, wait, immediate) {
+    var timeout
+    
+    return function(...args) {
+      var context = this
+      
+      var later = function () {
+        timeout = null
+        if (!immediate) func.apply(context, args)
+      }
+      
+      var callNow = immediate && !timeout
+      
+      clearTimeout(timeout)
+      timeout = setTimeout(later, wait)
+      
+      if (callNow) func.apply(context, args)
+    }
+  }
+   
+  var target = document.querySelectorAll('[data-anime]');
+  var animationClass = 'animate';
+   
+  function animeScroll() {
+    var windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    target.forEach((element) => {
+      if(windowTop > element.offsetTop) {
+        element.classList.add(animationClass, element)
+      } else {
+        element.classList.remove(animationClass, element)
+      }
+    })
+  }
+   
+  animeScroll();
+   
+  var handleScroll = debounce(animeScroll, 50);
+   
+  if(target.length) {
+    window.addEventListener('scroll', handleScroll);
+  }
+}
+
+if (typeof window !== 'undefined' && window) {
+	if (typeof module === 'object' && module.exports) {
+	  	module.exports = aniScrollable;
+	} else {
+	    window.aniScrollable = aniScrollable;
+	}
+}
