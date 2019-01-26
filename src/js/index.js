43d8457bf5a4e84fbaab00e83634372ac479422a
victufell <victor.fellype.a@gmail.com>
2019-01-26 20:20:10 -0200

    feat: added suport for window and import

--- src/js/index.js
+++ src/js/index.js
@@ -1,41 +1,50 @@
 const showllable = () => {
-  const debounce = function(func, wait, immediate) {
-    let timeout;
-    return function(...args) {
-      const context = this;
-      const later = function () {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
+  document.addEventListener('DOMContentLoaded', () => {
+    const debounce = function(func, wait, immediate) {
+      let timeout;
+      return function(...args) {
+        const context = this;
+        const later = function () {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
       };
-      const callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
     };
-  };
+    
+    const target = document.querySelectorAll('[data-anime]');
+    const animationClass = 'animate';
+    
+    function animeScroll() {
+      const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+      target.forEach(function(element) {
+        if((windowTop) > element.offsetTop) {
+          element.classList.add(animationClass);
+        } else {
+          element.classList.remove(animationClass);
+        }
+      })
+    }
+    
+    animeScroll();
+    
+    if(target.length) {
+      window.addEventListener('scroll', debounce(function() {
+        animeScroll();
+      }, 200));
+    }
+    
+  });
   
-  const target = document.querySelectorAll('[data-anime]');
-  const animationClass = 'animate';
-  
-  function animeScroll() {
-    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
-    target.forEach(function(element) {
-      if((windowTop) > element.offsetTop) {
-        element.classList.add(animationClass);
-      } else {
-        element.classList.remove(animationClass);
-      }
-    })
-  }
-  
-  animeScroll();
-  
-  if(target.length) {
-    window.addEventListener('scroll', debounce(function() {
-      animeScroll();
-    }, 200));
-  }
-
 }
 
-export default showllable;
+if (typeof window !== 'undefined' && window) {
+  if (typeof module === 'object' && module.exports) {
+	  module.exports = showllable;
+	} else {
+	  window.showlllable = showllable;
+  }
+} 
