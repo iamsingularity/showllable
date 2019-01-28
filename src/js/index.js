0c4be3093f9bc582f60f966dc2089b3e537adbc7
victufell <victor.fellype.a@gmail.com>
2019-01-28 14:18:37 -0200

    hotfix: refactory in cod

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,52 @@
-const showllable = () => {
-	const elements = document.querySelectorAll('[data-anime]');
-
-	const intersectionConfig = {
+const setIntersectionObserver = (elements) => {
+  
+  const intersectionConfig = {
 		root: null,
 		threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 1.0]
-	};
+  };
 
-	const myObserver = new IntersectionObserver((entries, observer) => {
+  const myObserver = new IntersectionObserver((entries, observer) => {
 		
 		if (entries.length === 0) {
 			observer.disconnect();
 		}
 
 		entries.forEach(entry => {
-			if (entry.intersectionRatio > 0) {
-				entry.target.classList.add('animate');
+      const target = entry.target.classList;
+      
+      if (entry.intersectionRatio > 0) {
+				target.add('animate');
 			} else {
-				entry.target.classList.remove('animate');
+				target.remove('animate');
 			}
 
 		});
 
 	}, intersectionConfig);
 
-
 	elements.forEach(element => {
 		myObserver.observe(element);
-	});
+  });
+  
+}
+
+const showllable = () => {
+	const elements = document.querySelectorAll('[data-anime]');
+  
+  if (('IntersectionObserver' in window)) {
+    setIntersectionObserver(elements);
+	} else {
+    elements.forEach(element => {
+      element.classList.add('animate')
+    });
+  }
 	
 }
 
-export default showllable;
+if (typeof window !== 'undefined' && window) {
+  if (typeof module === 'object' && module.exports) {
+	  module.exports = showllable;
+	} else {
+	  window.showlllable = showllable;
+  }
+}
